01740e0e2accc73a7eb06cccd0d1fd59737269c8
iamlive <86843363+Swapnayg@users.noreply.github.com>
2025-07-16 18:27:26 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // for dev only
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
+      },
+    ];
+  },
 };
 
 
